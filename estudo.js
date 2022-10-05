4b901139cd49b382043e11a65426c500532c4d1a
Lix <108944969+wygvsco@users.noreply.github.com>
2022-10-05 11:55:42 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,7 +1,7 @@
 let tabuada = 5
 
 function escreva(){
-    document.write("tabuada do" + tabuada + "<br>");
+    document.write("tabuada do " + tabuada + "<br>");
     document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
     document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
     document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
@@ -14,3 +14,21 @@ function escreva(){
     document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
 }
 
+let lista = ["Terra","Marte","Saturno","Jupiter","Vênus"];
+
+function mostraLista(){
+    document.write("Tamanho da lista " + lista.length + "<br>");
+
+    for(let i = 0; i < lista.length; i++){
+        document.write("Planeta " + lista[i]+ "<br>");
+    }
+}
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+        document.write ("Tabuada do =" + i + "<br>");
+        for (let j = 1; j <=10; j++){
+            document.write(1+" x " + j + " = " +(i*j)+ "<br>");
+        }
+    }
+}
